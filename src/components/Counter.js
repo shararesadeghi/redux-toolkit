069dcd7e64818bfa8db74408b2d5ab8b3aea57f7
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-09-25 15:56:48 +0330

    action.payload added

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector , useDispatch} from 'react-redux';
 //import store from '../app/store';
 import { selectCounter } from '../features/counter/counterSlice';
-import { increment, decrement } from '../features/counter/counterSlice';
+import { increment, decrement, incrementByAmount } from '../features/counter/counterSlice';
 
 const Counter = () => {
 
     //const counter = useSelector(store=>store.counter.counterValue)
     const counter = useSelector(selectCounter); 
      const dispatch = useDispatch();
+
+     const [value,setValue] = useState(2);
     return (
         <div>
             <h2>Counter is {counter}</h2>
             <button onClick={()=>dispatch(increment())}>Increment</button>
             <button onClick={()=>dispatch(decrement())}>Decrement</button>
+            <input type="number" value={value} onChange={e=>setValue(e.target.value)} />
+            <button onClick={()=>dispatch(incrementByAmount(+value))}>IncrementByAmount</button>
         </div>
     );
 };
--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -8,15 +8,18 @@ const counterSlice = createSlice({
     name:"counter",
     initialState,
     reducers:{
-        increment: (state,action)=>{
+        increment: (state)=>{
             state.counterValue++;
         },
-        decrement: (state,action)=>{
+        decrement: (state)=>{
             state.counterValue--;
         },
+        incrementByAmount:(state,action)=>{
+            state.counterValue += action.payload;
+        }
     },
 });
 
 export default counterSlice.reducer;
-export const {increment,decrement} = counterSlice.actions;
+export const {increment,decrement,incrementByAmount} = counterSlice.actions;
 export const selectCounter = (store)=>store.counter.counterValue;
