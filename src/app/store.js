c450c0c8b56114e437bdd80b78ffec518375d6c5
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-09-26 11:29:04 +0330

    addmiddleware

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,13 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 
 import counterReducer from '../features/counter/counterSlice';
 import numberReducer from '../features/number/numberSlice';
 
+import logger from 'redux-logger';
+
 const store = configureStore({
     reducer:{counter:counterReducer, number:numberReducer},
+    middelware : (getDefaultMiddleware)=>getDefaultMiddleware().concat(logger),
 });
 
 export default store;
